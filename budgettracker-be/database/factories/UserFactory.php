70cc00ebd486d691bc8dd137726a07b41e8a3d0e
Anja Jandrić <aj20200081@student.fon.bg.ac.rs>
2026-03-08 00:26:15 +0100

    Instaliran Laravel Framework za REST API

---
+++ budgettracker-be/database/factories/UserFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' =>  Hash::make('password'),
+            'user_type' => $this->faker->randomElement(['user', 'admin']),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+   
+}
